8934e12480ef93e78c320772f2ff604485552591
Ralph <git@ralph.ms>
2017-12-05 19:40:09 +0900

    Add project files

---
+++ app/src/main/java/ms/ralph/android/recyclerviewsample/MainActivity.java
@@ -0,0 +1,13 @@
+package ms.ralph.android.recyclerviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
